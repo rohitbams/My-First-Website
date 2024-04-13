779ec7bf98e8ea7b7e1b1907bbf6538339353a25
rohitbams <143954072+rohitbams@users.noreply.github.com>
2024-04-13 12:54:23 +0530

    create a special passward protected page

---
+++ css/script.js
@@ -0,0 +1,22 @@
+/*
+let buttonInput = document.querySelector('input');
+addEventListener("click", function buttonClick() {
+if (buttonInput === 'gone') {
+    window.location="../pages/lad.html";
+} else {
+    alert('You entered the wrong password, prepare for the consequences');
+}
+});
+*/
+
+document.getElementById('passwordForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+
+    var password = document.getElementById('password').value;
+
+    if (password === 'gone') {
+        window.location = '../pages/lad.html';
+    } else {
+        document.getElementById('message').textContent = 'Wrong';
+    }
+});
